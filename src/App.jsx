e8a9d811e215c0feab814ab4748aecc1c818aac9
srujana-malyala <malyalasrujana40@gmail.com>
2026-01-10 17:22:51 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Footer } from './components/Footer/Footer'
+import { Header } from './components/Header'
+
+function App() {
+ 
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+export const Header = ()=>{
+    return(
+        <>
+        <h1>Tech-Shop</h1>
+
+         <div className="icons">
+                <i className="fa-solid fa-magnifying-glass"></i>
+                <i className="fa-solid fa-cart-shopping"></i>
+
+            </div>
+        
+        </>
+    )
+}
